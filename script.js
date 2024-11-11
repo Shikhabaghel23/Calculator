1c2ec1636a9b3c3ba22853fc751b3258dba73197
Shikhabaghel23 <shikhabaghel23@navgurukul.org>
2024-11-11 15:53:02 +0530

    index.html

---
+++ script.js
@@ -0,0 +1,31 @@
+function appendNumber(number) {
+    const display = document.getElementById('display');
+    display.value += number;
+  }
+  
+  function appendOperator(operator) {
+    const display = document.getElementById('display');
+    const lastChar = display.value.slice(-1);
+    if (!['+', '-', '*', '/'].includes(lastChar)) {
+      display.value += operator;
+    }
+  }
+  
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+  
+  function deleteLast() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+  }
+  
+  function calculate() {
+    const display = document.getElementById('display');
+    try {
+      display.value = eval(display.value);
+    } catch {
+      display.value = 'Error';
+    }
+  }
+  
